7e0be103dd1f5992adb296403a2a8b0d9d9004b4
Garvit Agrawal <garvit.agrawal.che20@itbhu.ac.in>
2023-03-22 23:31:38 +0530

    Modified Navbar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import "./App.css";
-import Header from "./Title";
 import Nav from "./Nav";
 import Contests from "./Contests";
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import React from "react";
 import "./Nav.css";
 import QrCode from "./QrCode/QrCode";
-
 import PasswordGenerator from "./PasswordGenerator/PasswordGenerator";
-// import Header from "./Title";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="nav-bar">
-        <ul className="list-head">
-          <li className="list-item">
-            <a href="/home" element={<Contests />}>
-              Home
-            </a>
-          </li>
-          <li className="list-item">
-            <a href="/QrCode" element={<QrCode />}>
-              QR code
-            </a>
-          </li>
-          <li className="list-item">
-            <a href="/PassWord" element={<PasswordGenerator />}>
-              Password Generator
-            </a>
-          </li>
-          <li className="list-item">
-            <a href="/todo">ToDO List</a>
-          </li>
-        </ul>
-      </div>
-      <Nav />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Contests key="all" name={"all"} />} />
+          <Route
+            exact
+            path="/leet_code"
+            element={<Contests key="leet_code" name={"leet_code"} />}
+          />
+          <Route
+            exact
+            path="/codeforces"
+            element={<Contests key="codeforces" name={"codeforces"} />}
+          />
+          <Route
+            exact
+            path="/at_coder"
+            element={<Contests key="at_coder" name={"at_coder"} />}
+          />
+          <Route
+            exact
+            path="/code_chef"
+            element={<Contests key="code_chef" name={"code_chef"} />}
+          />
+          <Route
+            exact
+            path="/hacker_rank"
+            element={<Contests key="hacker_rank" name={"hacker_rank"} />}
+          />
+          <Route
+            exact
+            path="/kick_start"
+            element={<Contests key="kick_start" name={"kick_start"} />}
+          />
+          <Route exact path="/QrCode" element={<QrCode />} />
+          <Route exact path="/PassWord" element={<PasswordGenerator />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
