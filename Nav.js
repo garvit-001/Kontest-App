82c93d9a6143771a69a3fbf6e711aced6d1e0aab
Garvit Agrawal <91630225+garvit-001@users.noreply.github.com>
2023-03-22 23:48:09 +0530

    Add files via upload
    "Modified Navbar"

--- Nav.js
+++ Nav.js
@@ -1,53 +1,63 @@
 import React from "react";
 import "./Nav.css";
-import QrCode from "./QrCode/QrCode";
-import Contests from "./Contests";
-import PasswordGenerator from "./PasswordGenerator/PasswordGenerator";
-// import Header from "./Title";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Contests />} />
-        {/* <Route path="/about" element = {}/>
-      <Route path="/services" element = {}/> */}
-        <Route path="/QrCode" element={<QrCode />} />
-        <Route path="/PassWord" element={<PasswordGenerator />} />
-        {/* <Route path="/todo" element={<Todo />} /> */}
-      </Routes>
-    </BrowserRouter>
-
-    // <Router>
-    //   <section>
-    //     <div className="navbar">
-    //       <ul className="list-head">
-    //         {/* <li className="list-item">
-    //           <Link to="/">Home</Link>
-    //         </li>
-    //         <li className="list-item">
-    //           <Link to="/About">About</Link>
-    //         </li>
-
-    //         <li className="list-item">
-    //           {" "}
-    //           <Link to="/Services">Services</Link>
-    //         </li> */}
-
-    //         <li className="list-item">
-    //           <Link to="/QrCode">QR code</Link>
-    //         </li>
-    //       </ul>
-    //       {/* <Route exact path="/" component={Contests} /> */}
-    //       {/* <Route exact path="/About" component={About} />
-    //       <Route exact path="/Services" component={Services} /> */}
-    //       <Route exact path="/QrCode" component={QrCode} />
-    //     </div>
-    //   </section>
-    //   <Header />
-    //   <Contests />
-    // </Router>
+    <div className="nav-bar">
+      <ul className="list-head">
+        <li className="list-item">
+          <Link className="link" to="/">
+            Contest
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/codeforces">
+            CodeForces
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/leet_code">
+            Leetcode
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/at_coder">
+            AtCoder
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/code_chef">
+            CodeChef
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/hacker_rank">
+            HackerRank
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/kick_start">
+            KickStart
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/QrCode">
+            QR code
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/PassWord">
+            Password Generator
+          </Link>
+        </li>
+        <li className="list-item">
+          <Link className="link" to="/todo">
+            ToDO List
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
